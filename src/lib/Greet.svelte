d9e711734031fdfad65a3ba402577f3024aeb407
ppp <ppp.3ppj@gmail.com>
2023-11-09 00:40:18 +0700

    add: example to greet display data

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { invoke } from '@tauri-apps/api/tauri'
+
+  let name = ''
+  let greetMsg = ''
+
+  async function greet() {
+    console.log("Hiiii");
+    greetMsg = await invoke('greet', { name })
+  }
+</script>
+
+<div>
+  <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
+  <button on:click="{greet}">Greet</button>
+  <p>{greetMsg}</p>
+</div>
